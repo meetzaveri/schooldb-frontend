886c182b28e1b9f63d02047f9603441377c9de0a
meetzaveri <meethpatel96@gmail.com>
2018-09-03 17:22:25 +0530

    Added modal view to access student marksheet

--- src/components/teacher/index.js
+++ src/components/teacher/index.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { Grid, Col, Nav, NavItem, Table } from "react-bootstrap";
+import ModalBox from "./modal";
 
 const ListRender = props => {
   console.log("props.lists", props.lists);
@@ -10,32 +11,61 @@ const ListRender = props => {
       <td>{item.name}</td>
       <td>{item.email}</td>
       <td>{item.roll_no}</td>
+      <td>
+        <button onClick={() => props.handleOnClickModal(item)}>
+          View marksheet
+        </button>
+      </td>
     </tr>
   ));
   return render;
 };
 
-const Teacher = props => {
-  return (
-    <Fragment>
-      <Grid>
-        <h3>Student Details</h3>
-        <Table striped bordered condensed hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Roll no.</th>
-            </tr>
-          </thead>
-          <tbody>
-            <ListRender {...props} />
-          </tbody>
-        </Table>
-      </Grid>
-    </Fragment>
-  );
-};
+class Teacher extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showModal: false, showModalRecord: {} };
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+  handleShowModal = record => {
+    console.log("record on modal", record);
+    this.setState({ showModal: true, showModalRecord: record });
+  };
+  render() {
+    const { handleShowModal, handleCloseModal } = this;
+    return (
+      <Fragment>
+        {" "}
+        <Grid>
+          <h3>Student Details</h3>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>Sr no.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Roll no.</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ListRender
+                {...this.props}
+                handleOnClickModal={handleShowModal}
+              />
+              <ModalBox
+                state={this.state}
+                handleCloseModal={handleCloseModal}
+              />
+            </tbody>
+          </Table>
+        </Grid>
+      </Fragment>
+    );
+  }
+}
 
 export default Teacher;
